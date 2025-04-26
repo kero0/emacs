97e7ea7c2bfdc2a38511b70257989d9a623272e0
kero0 <kbakheat@gmail.com>
2025-04-26 11:38:58 -0400

    ditch garnix

--- flake.nix
+++ flake.nix
@@ -3,12 +3,10 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://kero0.cachix.org"
-      "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "kero0.cachix.org-1:uzu0+ZP6R1U1izim/swa3bfyEiS0TElA8hLrGXQGAbA="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     sandbox = false; # sandbox causing issues on darwin
   };
